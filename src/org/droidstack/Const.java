c5c114e98654054e1ae82098d625f60a640e6751
Felix Oghină <felix.oghina@gmail.com>
2010-07-08 22:57:42 +0300

    Added API key

--- src/org/droidstack/Const.java
+++ src/org/droidstack/Const.java
@@ -3,7 +3,7 @@ package org.droidstack;
 public final class Const {
 	
 	public final static String TAG = "Droidstack";
-	public final static String APIKEY = "";
+	public final static String APIKEY = "8eE4X4E2LEGgAjNRfVePkg";
 	
 	public final static String PREF_PAGESIZE = "pagesize";
 	public static final int DEF_PAGESIZE = 10;
