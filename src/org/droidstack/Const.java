2056d68f12f6cc7c86d2cf6994d57fa65008cc91
Felix Oghină <felix.oghina@gmail.com>
2010-07-14 17:54:57 +0300

    Increased timeout to 30 seconds to fix THE WEIRDEST FUCKING BUG EVER

--- src/org/droidstack/Const.java
+++ src/org/droidstack/Const.java
@@ -5,7 +5,7 @@ public final class Const {
 	public final static String TAG = "Droidstack";
 	public final static String APIKEY = "8eE4X4E2LEGgAjNRfVePkg";
 	
-	public final static int NET_TIMEOUT = 10000;
+	public final static int NET_TIMEOUT = 30000;
 	
 	public final static String PREF_PAGESIZE = "pagesize";
 	public static final int DEF_PAGESIZE = 10;
