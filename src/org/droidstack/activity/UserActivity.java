7eced276f81802510b87eed5fcf3e0b617721b4d
Felix Oghină <felix.oghina@gmail.com>
2010-11-07 17:08:12 +0200

    organized imports

--- src/org/droidstack/activity/UserActivity.java
+++ src/org/droidstack/activity/UserActivity.java
@@ -34,7 +34,6 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.Html;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
